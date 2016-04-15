1a73a20cb873760424998090fe09600d58c528f9
Tony DiCola <tony@tonydicola.com>
2016-04-15 01:11:48 +0000

    Add pypi metadata.

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
 setup(name              = 'Adafruit_BNO055',
       version           = '1.0.0',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Library for accessing the Bosch BNO055 absolute orientation sensor on a Raspberry Pi or Beaglebone Black.',
       license           = 'MIT',
+      classifiers       = classifiers,
       url               = 'https://github.com/adafruit/Adafruit_Python_BNO055/',
       dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.9.3'],
       install_requires  = ['Adafruit-GPIO>=0.9.3', 'pyserial'],
